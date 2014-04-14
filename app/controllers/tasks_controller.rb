00d33f1e979fafdea1fca4e034ecad30f59fbcaa
kelly becker & Anne Jhetson <student@epicodus.com>
2014-04-14 12:00:40 -0700

    adds ajax functionality for updating

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,8 @@
 class TasksController < ApplicationController
   def index
     @tasks = Task.all
+    @incomplete_tasks = Task.where(done: false)
+    @complete_tasks = Task.where(done: true)
   end
 
   def new
@@ -9,10 +11,21 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.create(task_params)
+    # redirect_to tasks_path
     respond_to do |format|
       format.html { redirect_to tasks_path }
       format.js
     end
+  end
+
+  # def edit
+  #   @task = Task.find(params[:id])
+  # end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(done: true)
+    redirect_to tasks_path
   end
 
   private
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,10 +2,10 @@ class Task < ActiveRecord::Base
 
   validates :name, :presence => true
 
-  after_create :set_false
+  # after_create :set_false
 
-   private
-     def set_false
-       self.update(done: false)
-     end
+  #  private
+  #    def set_false
+  #      self.update(done: false)
+  #    end
 end
